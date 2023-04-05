5ab76b561b90a24cb32e4767d4e3d862771e778a
Edward Wong <edward.wong@live.ca>
2023-04-04 20:24:35 -0400

    Added chicken drop + disappearing text to item pickups

--- Assets/Scripts/Player/pickUpItem.cs
+++ Assets/Scripts/Player/pickUpItem.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class pickUpItem : MonoBehaviour
 {
-    public enum itemType {coffee,chest};   
+    public enum itemType {coffee,chest,chicken};   
     private GameObject player;
     public itemType typeOfItem;
+    public TextMeshPro HPPrefab;
 
     void Start() 
     {
@@ -24,12 +26,22 @@ public class pickUpItem : MonoBehaviour
         if(typeOfItem==itemType.coffee)
         {
             player.GetComponent<PlayerDamageControl>().ChangeHealth(1);
+            TextMeshPro recoveryText = Instantiate(HPPrefab, gameObject.transform.position + new Vector3(0, 0.5f, -1), Quaternion.identity);
+            Destroy(recoveryText, 1.5f);
             Destroy(gameObject);
         }
 
         if(typeOfItem==itemType.chest)
         {
             GetComponent<Chest>().Open();
+        }
+
+        if (typeOfItem == itemType.chicken)
+        {
+            player.GetComponent<PlayerDamageControl>().IncreaseHealth(1);
+            TextMeshPro increaseText = Instantiate(HPPrefab, gameObject.transform.position + new Vector3(0, 0.5f, -1), Quaternion.identity);
+            Destroy(increaseText, 1.5f);
+            Destroy(gameObject);
         }
 
         
